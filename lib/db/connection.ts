1322673d939bcfea5f7e5a24b2ff6155841803c7
A5KET <asketinin@gmail.com>
2025-10-17 14:44:09 +0300

    Switched to neon database

--- lib/db/connection.ts
+++ lib/db/connection.ts
@@ -1,8 +1,3 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
+import { drizzle } from 'drizzle-orm/neon-http'
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-})
-
-export const db = drizzle({ client: pool })
+export const db = drizzle(process.env.DATABASE_URL!)
